35438c6ba8df77917eac97dfc0f681a519784431
Chhavi Kumari <kumarichhavi60@gmail.com>
2026-09-14 15:25:35 +0530

    Time: 594 ms (5%), Space: 0B (100%) - LeetHub

--- 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -1,7 +1,11 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- UPDATE salary
+-- SET sex = CASE 
+--     WHEN sex = 'f' THEN 'm'
+--     WHEN sex = 'm' THEN 'f'
+--     ELSE sex 
+-- END;
+
+
 UPDATE salary
-SET sex = CASE 
-    WHEN sex = 'f' THEN 'm'
-    WHEN sex = 'm' THEN 'f'
-    ELSE sex 
-END;
+SET sex = IF(sex = 'f', 'm', 'f');
