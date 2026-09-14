c9d4fab35629eff26ee70142466bbb8115979398
Chhavi Kumari <kumarichhavi60@gmail.com>
2026-09-14 15:00:33 +0530

    Time: 0 ms (100%), Space: 52.1 MB (95.55%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,10 +1,36 @@
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& prices, int money) {
+
+//         sort(prices.begin(), prices.end());
+    
+//         int min_cost = prices[0] + prices[1];
+
+//         if (min_cost <= money) {
+//             return money - min_cost;
+//         }
+//         return money;
+//     }
+// };
+
+
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
+        int n = prices.size();
+        int min_cost = INT_MAX; 
 
-        sort(prices.begin(), prices.end());
-    
-        int min_cost = prices[0] + prices[1];
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = i + 1; j < n; j++) {
+                
+                int current_cost = prices[i] + prices[j];
+                
+                if (current_cost < min_cost) {
+                    min_cost = current_cost;
+                }
+            }
+        }
 
         if (min_cost <= money) {
             return money - min_cost;
